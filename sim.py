bf63b3a236eafc04f5955f6d7744b443fbbd4c05
Philipp Thalhammer <philipp.thalhammer@fau.de>
2020-03-22 03:18:17 +0100

    Bug fixes & multiple runs

--- sim.py
+++ sim.py
@@ -22,7 +22,7 @@ def rand_strat(eyes,low):
 
 # Simple test strategy which stops above 8
 def test_strat(eyes,low ):
-    if (eyes > 8 and eyes < low): 
+    if ( eyes < 7): 
         return 1
     else: 
         return 0
@@ -53,61 +53,79 @@ def find_low():
     return low
 
 def mark_loser():
-    i = 0
     low = find_low()
     for pl in players:
         if pl.eyes == low: 
             pl.marks += 1
             if p: print(pl.name + " bekommt nen Strich. ")
 
+def reset():
+    for pl in players:
+        pl.eyes = 0
+        pl.marks = 0 
 
 def punish_loser():
     for pl in players:
         if pl.eyes >= 16 or pl.marks >= 3: 
-            print(pl.name +" lost the game!")        
+            if p: print(pl.name +" lost the game!")     
+            return pl.name   
 
 def throw_dice():
-    return randrange(1,6)
+    return randrange(1,7)
 
 
 
 players.append(player(test_strat,cont_stat,"Ulf")) # The chosen one
+
 for i in range(n_pl-1):
     players.append(player(rand_strat,cont_stat,names[i]))
 
 
 # Main loop for one Round of playing 
 
-cur_eyes = 0
-c1 = 1
-while(True):                                      # Play until somebody lost
-    if p: print("Starting Round "+ str(c1) )
-    for  pl in players:   
-        low = find_low()                          # Play one round
-        pl.eyes = throw_dice()+throw_dice()       # Every player start ny throwing 2 times
+
+def play():
+
+    c1 = 1
+    while(True):                                      # Play until somebody lost
+        if p: print("Starting Round "+ str(c1) )
+        for  pl in players:   
+            low = find_low()                          # Play one round
+            pl.eyes = throw_dice()+throw_dice()       # Every player start ny throwing 2 times
         
-        if pl.st1(pl.eyes,low):                   # Will the player choose to continue 
-           pl.eyes += throw_dice() + throw_dice() # If so throw two more times 
-        if pl.eyes > 16:                          # Stop if he has over-thrown
-            if p: print(pl.name + " hat "+ str(pl.eyes) + " geworfen" )            
-            break
-        else: pl.eyes = pl.st2(pl.eyes)           # Otherwise continue with sec strat
+            if pl.st1(pl.eyes,low):                   # Will the player choose to continue 
+                pl.eyes += throw_dice() + throw_dice() # If so throw two more times 
+            if pl.eyes > 16:                          # Stop if he has over-thrown
+                if p: print(pl.name + " hat "+ str(pl.eyes) + " geworfen" )            
+                break
+            else: pl.eyes = pl.st2(pl.eyes)           # Otherwise continue with sec strat
     	
-        if p: print(pl.name + " hat "+ str(pl.eyes) + " geworfen" )
+            if p: print(pl.name + " hat "+ str(pl.eyes) + " geworfen" )
 
-    if find_max() > 16:  
-        if p: print("Überworfen!")                # After the round: Did someone over-throw
-        punish_loser()
-        break
-    else: mark_loser()                            # If not, mark losers
+        if find_max() > 16:  
+            if p: print("Überworfen!")                # After the round: Did someone over-throw
+            loser = punish_loser()
+            reset()
+            break
+        else: mark_loser()                            # If not, mark losers
 
-    if p: print(" \n")
+        if p: print(" \n")
 
-    if max_marks() >= 3: 
-        punish_loser()                     # Does anyone reach three marks
-        break
+        if max_marks() >= 3: 
+            loser = punish_loser()  
+            reset()                   # Does anyone reach three marks
+            break
+        c1 += 1 
+    return loser
 
-    c1 += 1 
 
 
+p=0
+loosers = []
+for i in range (1000):
+  loosers.append(play())
+
+
+for pl in players:
+    print(pl.name + " hat " + str(loosers.count(pl.name)) + " mal verloren")
 
